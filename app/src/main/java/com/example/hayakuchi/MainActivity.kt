aac65e8609f46a6832734a20ec25ac28ade98838
morishy1257 <127572407+morishy1257@users.noreply.github.com>
2025-11-03 22:41:16 +0900

    Change Logic

--- app/src/main/java/com/example/hayakuchi/MainActivity.kt
+++ app/src/main/java/com/example/hayakuchi/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.example.hayakuchi
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hayakuchi.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), View.OnClickListener, TextToSpeech.OnInitListener {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var tts: TextToSpeech
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        tts = TextToSpeech(this@MainActivity, this@MainActivity)
+
+        binding.btnFast.setOnClickListener(this)
+        binding.btnNormal.setOnClickListener(this)
+        binding.btnSlow.setOnClickListener(this)
+    }
+
+    override fun onInit(status: Int){
+
+    }
+
+    override fun onClick(v: View){
+
     }
 }
